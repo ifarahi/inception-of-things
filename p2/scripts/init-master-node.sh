e26e32457d1c64e5ce68199c12f7a3ea8374ad55
farahi ismail <dr0farahi@gmail.com>
2023-04-05 18:18:46 +0000

    wip: host 3 applications in a k3s cluster and expose it with ingress

---
+++ p2/scripts/init-master-node.sh
@@ -0,0 +1,12 @@
+
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --flannel-iface=eth1" sh -s -
+/usr/local/bin/kubectl create -f /vagrant/config/app-one.yaml
+/usr/local/bin/kubectl create -f /vagrant/config/app-one-service.yaml
+
+/usr/local/bin/kubectl create -f /vagrant/config/app-two.yaml
+/usr/local/bin/kubectl create -f /vagrant/config/app-two-service.yaml
+
+/usr/local/bin/kubectl create -f /vagrant/config/app-three.yaml
+/usr/local/bin/kubectl create -f /vagrant/config/app-three-service.yaml
+
+/usr/local/bin/kubectl create -f /vagrant/config/ingress.yaml
