7201dda265ebbef6cb7202fcac4bc91c69a2cd6d
farahi ismail <dr0farahi@gmail.com>
2023-04-26 20:20:46 +0100

    wip: p3 add new box

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -18,6 +18,8 @@ apt-get update -y
 
 apt-get install -y --fix-missing docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+sudo usermod -aG docker vagrant
+
 echo "install docker - END"
 # install docker END
 
@@ -25,10 +27,34 @@ echo "install docker - END"
 echo "install k3d - START"
 
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | TAG=v5.0.0 bash
-curl -o /usr/local/bin -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -o /usr/local/bin/kubectl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 ln -s /usr/local/bin/kubectl /usr/local/bin/k
 chmod +x /usr/local/bin/kubectl
 chmod +x /usr/local/bin/k
 
 echo "install k3d - END"
 # install k3d - END
+
+# create cluster - START
+echo "create cluster - START"
+
+k3d cluster create iot-cluster
+
+echo "waiting for k3d to be ready"
+sleep 60
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+echo "create cluster - END"
+# create cluster - END
+
+
+# install argocd - START
+echo "install argocd - START"
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# install argocd - END
+echo "install argocd - END"
