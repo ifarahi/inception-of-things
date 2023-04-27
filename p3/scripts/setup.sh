b3cb67e5741de3ee8e13294a549b0c85d9c1f2f6
farahi ismail <dr0farahi@gmail.com>
2023-04-27 19:11:05 +0100

    chore(p3): start using ubuntu virtualbox instead of vagrant

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -1,9 +1,11 @@
 # install docker - START
-echo "install docker - START"
 
-apt update -y
+apt-get update
 
-apt-get install -y ca-certificates curl gnupg
+apt-get install \
+    ca-certificates \
+    curl \
+    gnupg
 
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -14,47 +16,36 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt-get update -y
+apt-get update
 
-apt-get install -y --fix-missing docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo usermod -aG docker vagrant
+usermod -aG docker vagrant
 
-echo "install docker - END"
-# install docker END
+newgrp docker
+
+# install docker - END
 
 # install k3d - START
-echo "install k3d - START"
-
+# install kubctl - START
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | TAG=v5.0.0 bash
-curl -o /usr/local/bin/kubectl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-ln -s /usr/local/bin/kubectl /usr/local/bin/k
-chmod +x /usr/local/bin/kubectl
-chmod +x /usr/local/bin/k
-
-echo "install k3d - END"
-# install k3d - END
-
-# create cluster - START
-echo "create cluster - START"
 
 k3d cluster create iot-cluster
+# TODO - wait for cluster to be ready
 
-echo "waiting for k3d to be ready"
-sleep 60
+# install k3d - END
 
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+curl -o /usr/local/bin/kubectl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x /usr/local/bin/kubectl
 
-echo "create cluster - END"
-# create cluster - END
-
+# install kubctl - END
 
 # install argocd - START
-echo "install argocd - START"
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+# TODO - wait for argocd to be ready
 # install argocd - END
-echo "install argocd - END"
+
+
